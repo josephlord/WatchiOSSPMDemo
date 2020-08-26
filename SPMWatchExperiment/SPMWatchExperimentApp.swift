ede6f7bcbf0dac12f96c661ca4e9934b59f8c7af
Joseph Lord <joseph@human-friendly.com>
2020-08-26 16:21:25 +0100

    Initial Commit

---
+++ SPMWatchExperiment/SPMWatchExperimentApp.swift
@@ -0,0 +1,17 @@
+//
+//  SPMWatchExperimentApp.swift
+//  SPMWatchExperiment
+//
+//  Created by Joseph on 26/08/2020.
+//
+
+import SwiftUI
+
+@main
+struct SPMWatchExperimentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
